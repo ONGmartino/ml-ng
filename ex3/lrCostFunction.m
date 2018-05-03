58bb13e8a9c264795e146a57bac40e38cfc067f9
Ubuntu <ubuntu@ip-172-31-46-128.us-east-2.compute.internal>
2018-05-03 21:53:07 +0000

    ok the cost function?

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -37,12 +37,13 @@ grad = zeros(size(theta));
 %
 
 
+pos = -y .* log(sigmoid(X * theta));
+neg = (1-y) .* log(1 - sigmoid(X * theta));
 
+J = (1/m) * sum(pos - neg) * (lambda/(2*m)) * sum(theta(2:end).^2);
 
-
-
-
-
+reg = (lambda / m) * ([0; theta(2:end)]);
+grad = (1/m) * X' * (sigmoid(X*theta) -y) + reg;
 
 
 % =============================================================
