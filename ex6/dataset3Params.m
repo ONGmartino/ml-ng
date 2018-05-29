3a6c439d6120a9b1980bf62c95ebf919efadf95f
Ubuntu <ubuntu@ip-172-31-46-128.us-east-2.compute.internal>
2018-05-29 20:35:43 +0000

    first half working

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,10 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+endless = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]; error = 1
 
+for i=1:8
+    for j=1:8
 
+        c = endless(i); s = endless(j);
 
+        model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+        e = mean(double(svmPredict(model, Xval) ~= yval));
 
+        if e < error
+            C = c; sigma = s; error = e;
+    end
+end
 
 
 % =========================================================================
